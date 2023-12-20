05e180ec27d2af0018d250bc4e5dc6907cc73862
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-12-20 22:58:43 +0200

    Update EmployersManagement.cpp

--- C++ Programs/EmployersManagement/EmployersManagement/EmployersManagement.cpp
+++ C++ Programs/EmployersManagement/EmployersManagement/EmployersManagement.cpp
@@ -1,20 +1,188 @@
-// EmployersManagement.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <limits>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+struct Employee {
+    string name;
+    int id;
+    float percentage;
+    string position;
+    int age;
+};
+
+const int MAX_EMPLOYEES = 50;
+int currentID = 1; // Starting ID
+
+void writeToFile(Employee emp[], int size, const string& filename) {
+    ofstream fileOut(filename);
+    for (int i = 0; i < size; ++i) {
+        fileOut << emp[i].name << ","
+            << emp[i].id << ","
+            << emp[i].percentage << ","
+            << emp[i].position << ","
+            << emp[i].age << endl;
+    }
+    fileOut.close();
+    cout << "The file is saved." << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void readFromFile(Employee emp[], int& size, const string& filename) {
+    ifstream fileIn(filename);
+    string line;
+    size = 0;
+    if (!fileIn) {
+        cout << "The file is not loaded." << endl;
+    }
+    else {
+        while (getline(fileIn, line) && size < MAX_EMPLOYEES) {
+            istringstream iss(line);
+            getline(iss, emp[size].name, ',');
+            iss >> emp[size].id;
+            iss.ignore();
+            iss >> emp[size].percentage;
+            iss.ignore();
+            getline(iss, emp[size].position, ',');
+            iss >> emp[size].age;
+            size++;
+        }
+        currentID = size > 0 ? emp[size - 1].id + 1 : 1; // Update currentID based on the last employee's ID
+        fileIn.close();
+        cout << "The file is loaded." << endl;
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+void addEmployee(Employee emp[], int& size) {
+    if (size >= MAX_EMPLOYEES) {
+        cout << "Maximum capacity reached." << endl;
+        return;
+    }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input buffer
+    cout << "Enter employee full name: ";
+    getline(cin, emp[size].name);
+    emp[size].id = currentID++; // Assign and increment the current ID
+    cout << "Enter percentage: ";
+    cin >> emp[size].percentage;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer before reading position
+    cout << "Enter position: ";
+    getline(cin, emp[size].position);
+    cout << "Enter age: ";
+    cin >> emp[size].age;
+    size++;
+}
+
+void listEmployees(Employee emp[], int size) {
+    if (size == 0) {
+        cout << "No employees to display." << endl;
+        return;
+    }
+
+    // Display table header
+    cout << left
+        << setw(5) << "No."
+        << setw(20) << "Name"
+        << setw(10) << "ID"
+        << setw(10) << "%"
+        << setw(20) << "Position"
+        << setw(5) << "Age" << endl;
+    cout << string(70, '-') << endl; // Draw a line for the table header
+
+    // Display each employee's information in a row
+    for (int i = 0; i < size; ++i) {
+        cout << left
+            << setw(5) << i + 1
+            << setw(20) << emp[i].name
+            << setw(10) << emp[i].id
+            << setw(10) << emp[i].percentage
+            << setw(20) << emp[i].position
+            << setw(5) << emp[i].age << endl;
+    }
+}
+
+
+void editEmployee(Employee emp[], int size) {
+    int number;
+    cout << "Enter employee number to edit: ";
+    cin >> number;
+    if (number < 1 || number > size) {
+        cout << "Invalid employee number!" << endl;
+        return;
+    }
+    number--; // Adjust for zero-based indexing
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input buffer
+    cout << "Editing Employee: " << emp[number].name << endl;
+    cout << "Enter new name: ";
+    getline(cin, emp[number].name);
+    cout << "Enter new percentage: ";
+    cin >> emp[number].percentage;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer before reading position
+    cout << "Enter new position: ";
+    getline(cin, emp[number].position);
+    cout << "Enter new age: ";
+    cin >> emp[number].age;
+}
+
+
+void deleteEmployee(Employee emp[], int& size) {
+    int number;
+    cout << "Enter employee number to delete: ";
+    cin >> number;
+    if (number < 1 || number > size) {
+        cout << "Invalid employee number!" << endl;
+        return;
+    }
+    number--; // Adjust for zero-based indexing
+    for (int i = number; i < size - 1; i++) {
+        emp[i] = emp[i + 1];
+    }
+    size--;
+}
+
+int main() {
+    Employee employees[MAX_EMPLOYEES];
+    int numEmployees = 0;
+    int choice;
+    readFromFile(employees, numEmployees, "db_devshare.txt");
+    do {
+        cout << "1. Add Employee\n";
+        cout << "2. List Employees\n";
+        cout << "3. Edit Employee\n";
+        cout << "4. Delete Employee\n";
+        cout << "5. Save to File\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            addEmployee(employees, numEmployees);
+            break;
+        case 2:
+            listEmployees(employees, numEmployees);
+            break;
+        case 3:
+            editEmployee(employees, numEmployees);
+            break;
+        case 4:
+            deleteEmployee(employees, numEmployees);
+            break;
+        case 5:
+            writeToFile(employees, numEmployees, "db_devshare.txt");
+            break;
+        case 0:
+            writeToFile(employees, numEmployees, "db_devshare.txt");
+            cout << "Exiting the program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
